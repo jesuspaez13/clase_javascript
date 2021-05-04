39b8a818553fedd8ed4502476b2e180b8da766ab
Jesus Paez <jpaez13@cuc.edu.co>
2021-05-03 20:40:14 -0500

    Comentarios, sentencias, variables, constantes y uso de let

---
+++ script.js
@@ -0,0 +1,82 @@
+
+// Hola mundo
+//alert('Hola Mundo');
+
+// Comentarios
+
+//Comentario de línea
+/* Comentario de mas 
+de una línea
+*/
+
+
+
+
+// Sentencias
+
+//alert('Hola'); alert('Mundo');
+
+//alert('Hola'); 
+//alert('Mundo');
+
+
+// inserción automática de punto y coma
+
+//alert('Hola')
+//alert('Mundo')
+
+// Variables
+
+let mensaje;
+mensaje = "Hola Mundo"
+//alert(mensaje)
+
+let c=5, d=2, j = 3;
+
+let a = 5;
+let b = 6;
+let suma = a + b;
+console.log("El resultado de la suma es: ",suma);
+
+b = 7
+console.log(b)
+
+let nombre = "Roberto";
+let nombre_dos = nombre;
+
+nombre_dos = "Carlos";
+console.log("El nombre uno es: ",nombre);
+console.log("El nombre dos es: ", nombre_dos);
+
+
+// Nombrar variables con camel case
+nombreUno = "Roberto";
+nombreDos = "Carlos";
+
+// Nombrar como underscore
+nombre_uno = "Roberto";
+nombre_dos = "Carlos";
+
+// nombres no comunes
+let $ = 1;
+let _ = 2;
+
+//no puede nombrar variables
+
+/*let 1a = 3;
+let primer-nombre = 4;*/
+
+"use strict"
+mensaje = "Hola Mundo";
+
+
+// Constantes
+
+const PI = 3.141624;
+const COLOR_ROJO = "#FFFFF";
+pi_dos = pi;
+pi_dos = 7;
+console.log(pi);
+console.log(pi_dos);
+
+const id_account = get_id_account(45); 
